e69e066afa9077c6374affa5a33f803307c7203c
Ulu-pro <web.ulu7@gmail.com>
2023-04-30 16:36:54 +0500

    Configuration constants

---
+++ src/config.rs
@@ -0,0 +1,3 @@
+pub const API_URL: &str = "https://api.pexels.com/v1/search?query=nature";
+pub const API_KEY: &str = "563492ad6f9170000100000151ef63ed6e3a4420ab3f82b55caa9d9e";
+pub const PHOTO_PATH: &str = "bg.jpg";
